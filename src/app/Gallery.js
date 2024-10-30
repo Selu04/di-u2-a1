5e3a3d70f6d377f53bf6ffbeaff1e1824fd8f437
Selu <joseluispermag@gmail.com>
2024-10-30 10:13:20 +0100

    UPD: completo

--- src/app/Gallery.js
+++ src/app/Gallery.js
@@ -1,21 +1,12 @@
-// ¡Muéveme a Profile.js!
-export function Profile() {
-    return (
-      <img
-        src="https://i.imgur.com/QIrZWGIs.jpg"
-        alt="Alan L. Hart"
-      />
-    );
-  }
-  
-  export default function Gallery() {
-    return (
-      <section>
-        <h1>Científicos increíbles</h1>
-        <Profile />
-        <Profile />
-        <Profile />
-      </section>
-    );
-  }
+import Profile from "./Profile";
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Científicos increíbles</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
   
--- src/app/TodoList.js
+++ src/app/TodoList.js
@@ -1,5 +1,6 @@
 const person = {
     name: 'Gregorio Y. Zara',
+    src: "https://i.imgur.com/7vQD0fPs.jpg",
     theme: {
       backgroundColor: 'black',
       color: 'pink'
@@ -9,16 +10,16 @@ const person = {
   export default function TodoList() {
     return (
       <div style={person.theme}>
-        <h1>Tareas pendientes de {person}</h1>
+        <h1>Tareas pendientes de {person.name}</h1>
         <img
           className="avatar"
-          src="https://i.imgur.com/7vQD0fPs.jpg"
+          src= {person.src}
           alt="Gregorio Y. Zara"
         />
         <ul>
-        <li>Mejorar el videoteléfono</li>
-        <li>Preparar clases de aeronáutica</li>
-        <li>Trabajar en el motor de alcohol</li>
+          <li>Mejorar el videoteléfono</li>
+          <li>Preparar clases de aeronáutica</li>
+          <li>Trabajar en el motor de alcohol</li>
         </ul>
       </div>
     );
